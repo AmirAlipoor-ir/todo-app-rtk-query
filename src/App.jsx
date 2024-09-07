b8c6bbe08aa4882aecf6ee1afb9e37de3cf37f82
Amir <amir2005alipoor@gmail.com>
2024-09-07 09:50:38 +0330

    add todo in apiSlice added

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,40 @@
 import { useState } from "react";
-import { useGetTodosQuery } from "./feature/todo/apiSlice";
+import { useAddTodosMutation, useGetTodosQuery } from "./feature/todo/apiSlice";
 
 function App() {
   const [title, setTitle] = useState([]);
 
   const { data, error, isLoading } = useGetTodosQuery();
+  const [createTodo] = useAddTodosMutation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTodo(title);    
     setTitle("")
+    
   };
 
   return (
-    <>
+    <div>
+      <h1>Add todo</h1>
       <div>
-        <h1>Add todo</h1>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            />
-            <button type="submit">add todo</button>
-          </form>
-        </div>
-
-
-
-        <h1>todo list</h1>
-        <div>
-          {data?.map((item) => {
-            return <p key={item.id}>{item.title}</p>;
-          })}
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <button type="submit">add todo</button>
+        </form>
       </div>
-    </>
+
+      <h1>todo list</h1>
+      <div>
+        {data?.map((item) => {
+          return <p>{item.title}</p>;
+        })}
+      </div>
+    </div>
   );
 }
 
--- src/feature/todo/apiSlice.js
+++ src/feature/todo/apiSlice.js
@@ -3,11 +3,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  tagTypes: ["todos"],
   endpoints: (builder) => ({
     getTodos: builder.query({
-      query: () => "/todos",
+      query: () => ({ url: "/todos", method: "GET" }),
+      providesTags: ["todos"],
+    }),
+    addTodos: builder.mutation({
+      query: (title) => ({
+        url: "/todos",
+        method: "POST",
+        body: {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      }),
+      invalidatesTags: ["todos"],
     }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const { useGetTodosQuery, useAddTodosMutation } = apiSlice;
